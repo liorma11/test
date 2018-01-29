e9395b5424f95b27e7142f5827fb0d72182369be
bvautour <vautour.brad@gmail.com>
2018-01-29 15:24:59 -0800

    prep for output

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: bvautour <vautour.brad@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 10:05:55 by bvautour          #+#    #+#             */
-/*   Updated: 2018/01/26 14:34:04 by bvautour         ###   ########.fr       */
+/*   Updated: 2018/01/29 15:24:13 by bvautour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
+
+void	handle(t_list *list, int is_dir)
+{
+	t_list *trav;
+	trav = list;
+	while (trav)
+	{
+		if (is_dir)
+		{
+			printf("dirput\n");
+			//dirput(trav);
+		}
+		else
+		{
+			printf("itemput\n");
+			//itemput(trav);
+		}
+		trav = trav->next;
+	}
+	if (list)
+	{
+		printf("list exists time to free with listdel\n");
+	}
+
+}
 
 void	*get_name(t_list *elem)
 {
@@ -34,6 +59,11 @@ int	main(int ac, char **av)
 	ft_lstsort(&(ls.errors), &cmp_asc, &get_name);
 	lssort(&ls, &ls.dirs);
 	lssort(&ls, &ls.items);
-	unit(&ls);
+	handle(ls.errors, 0);
+	if (ls.items && ls.opts.all)
+		printf("all is called\n");
+	handle(ls.items, 0);
+	handle(ls.dirs, 1);
+	//unit(&ls);
 	return (0);
 }
